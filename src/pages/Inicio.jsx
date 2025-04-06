f86749e3d378d7e5b3f81234f56c173bc388a6f4
Federico Nova <fedenova8@gmail.com>
2025-04-06 13:10:42 -0300

    Creamos Inicio.jsx. También Formulario.jsx y Tabla.jsx, que se llaman desde Inicio.jsx

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Formulario from '../components/Formulario'
+import Tabla from '../components/Tabla'
+
+const Inicio = () => {
+
+    return (
+    <>
+        <Formulario/>
+        <hr />
+        <Tabla/>
+    </>
+    )
+}
+
+export default Inicio
