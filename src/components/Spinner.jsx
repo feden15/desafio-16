01cf8f3d0255133a40524b3f44adc85af5af8c3b
Federico Nova <fedenova8@gmail.com>
2025-04-06 14:21:54 -0300

    Damos por finalizado Desafío 16

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,11 @@ import './Spinner.css'
 const Spinner = () => {
 
     return (
-        <span className="loader"></span>
+        <div>
+            <span className="loader"></span>
+            <h3>No puedo usar MockAPI</h3>
+            <p>Tengo cargada la base del Integrador y solo se permite un proyecto a la vez</p>
+        </div>
     )
 
 }
