6797f1029e435acf69be8c3b2dceaa81ce57767a
Federico Nova <fedenova8@gmail.com>
2025-04-06 14:00:22 -0300

    Agregamos un Spinner genérico para cuando no se hayan cargado los usuarios y lo agregamos a TablaUsuarios. También agregamos el modal y las funciones handleVer y cerrarModal

--- src/components/TablaUsuarios.jsx
+++ src/components/TablaUsuarios.jsx
@@ -1,23 +1,60 @@
+import { useState } from 'react'
 import Filas from './Filas'
+import Spinner from './Spinner'
 
-const TablaUsuarios = () => {
+const TablaUsuarios = ({ usuarios, borrarUsuario, setUsuarioAEditar }) => {
+
+    const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null)
+    const [mostrarModal, setMostrarModal] = useState(false)
+
+    const handleVer = (usuarios) => {
+        setUsuarioSeleccionado(usuarios)
+        setMostrarModal(true)
+    }
+
+    const cerrarModal = () => {
+        setMostrarModal(false)
+        setUsuarioSeleccionado(null)
+    }
 
     return (
         <>
-            <table className="mt-5 w-full text-sm text-center text-gray-500">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-200">
-                    <tr>
-                        <th className="px-6 py-3">Nombre</th>
-                        <th className="px-6 py-3">Apellido</th>
-                        <th className="px-6 py-3">Edad</th>
-                        <th className="px-6 py-3">Puesto</th>
-                        <th className="px-6 py-3">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <Filas />
-                </tbody>
-            </table>
+            {usuarios ? (
+                <table className="mt-5 w-full text-sm text-center text-gray-500">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-200">
+                        <tr>
+                            <th className="px-6 py-3">Nombre</th>
+                            <th className="px-6 py-3">Apellido</th>
+                            <th className="px-6 py-3">Edad</th>
+                            <th className="px-6 py-3">Puesto</th>
+                            <th className="px-6 py-3">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            usuarios.map((usuarios) => (
+                                <Filas
+                                    usuarios={usuarios}
+                                    key={usuarios.id}
+                                    borrarUsuario={borrarUsuario}
+                                    setUsuarioAEditar={setUsuarioAEditar}
+                                    verUsuario={handleVer}
+                                />
+                            ))
+                        }
+                    </tbody>
+                </table>
+            ) : (
+                <div className="w-max m-auto mt-10">
+                    <Spinner />
+                </div>
+            )}
+            {mostrarModal && (
+                <ModalUsuario
+                    usuarios={usuarioSeleccionado}
+                    cerrarModal={cerrarModal}
+                />
+            )}
         </>
     )
 }
