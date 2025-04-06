a02773c6b1d2101a34bd081a2ae26d89500f7b1b
Federico Nova <fedenova8@gmail.com>
2025-04-06 14:34:36 -0300

    Agregamos estilo al h3 del Spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -5,7 +5,7 @@ const Spinner = () => {
     return (
         <div>
             <span className="loader"></span>
-            <h3>No puedo usar MockAPI</h3>
+            <h3 className="text-center font-bold my-2">No puedo usar MockAPI</h3>
             <p>Tengo cargada la base del Integrador y solo se permite un proyecto a la vez</p>
         </div>
     )
